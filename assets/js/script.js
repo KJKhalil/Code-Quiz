07d54c350b0163c69f7d8dce3adfb0b54559e5af
Khalil <khalil.j.khalil2@gmail.com>
2022-09-12 13:37:45 -0400

    Finished Most of Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,26 +31,26 @@ class Question {
     }
 
     isCorrect(answers) {
-        return this.correctAnswer === answer;
+        return this.correctAnswer === answers;
     }
 }
 
 function askQuestion() {
 
     if (quiz.quizEnd()) {
-        displayScores();
+        scores();
     } else {
         let questionElement = document.getElementById('question');
         questionElement.innerHTML = quiz.getQuestions().text;
 
         let answers = quiz.getQuestions().answers;
         for (let i = 0; i < answers.length; i++) {
-            let answerElement = documentElementById('answer' + i);
+            let answerElement = document.getElementById('answer' + i);
             answerElement.innerHTML = answers[i];
             userChoice('btn' + i, answers[i]);
         }
 
-        showProgress();
+        progress();
     }
 }
 
@@ -68,6 +68,17 @@ function progress() {
     progress.innerHTML = `Question ${currentQuestion} of ${quiz.questions.length}`;
 }
 
+function scores() {
+    let quizEndHTML =
+        `   
+            <h1>Quiz Finished</h1>
+            <h2 id='score'> You Scored: ${quiz.score} of ${quiz.questions.length}</h2>
+            <div class='repeatquiz'> <a href='index.html'>Retake Quiz</a></div>
+        `;
+    let quizElement = document.getElementById('quiz');
+    quizElement.innerHTML = quizEndHTML;
+}
+
 let questions = [
     new Question('What does HTML stand for?', ['Hypertext Markup Language', 'Hybridtext Markup Langue', 'Hypertext Makeup Language',
     'Hyrbridtext Makeup Language'], 'Hypertext Markup Language'),
@@ -75,10 +86,10 @@ let questions = [
     new Question('What does CSS stand for?', ['Creative Style Sheets', 'Cascading Style Slang', 'Creative Style Slang',
     'Cascading Style Sheets'], 'Cascading Style Sheets'),
 
-    new Question('What is an array in Javascript' ['A collection of functions', 'An ordered list of values', 'A list of classes',
+    new Question('What is an array in Javascript', ['A collection of functions', 'An ordered list of values', 'A list of classes',
     'An ordered list of ids'], 'An ordered list of values'),
 
-    new Question('What are HTML, CSS, and Javascript used for?' ['Crying yourself to sleep', 'Back end development',
+    new Question('What are HTML, CSS, and Javascript used for?', ['Crying yourself to sleep', 'Back end development',
     'Accounting', 'Front end development'], 'Front end development')
 ];
 
@@ -86,9 +97,24 @@ let quiz = new Quiz(questions);
 
 askQuestion();
 
-function scores() {
-    let quizEnd =
-    `
-        
+let timer = 5;
+let timeInMinutes = timer * 60 * 60;
+quizTime = timeInMinutes / 60;
+
+let time = document.getElementById('time');
+
+function startTimer() {
+    let quizTimer = setInterval(function() {
+        if (quizTimer <= 0) {
+            clearInterval(quizTimer);
+            scores();
+        } else {
+            quizTimer--;
+            let second = Math.floor(quizTime % 60);
+            let minute = Math.floor(quizTime / 60) % 60;
+            time.innerHTML = `TIME: ${minute} : ${second}`;
+        }
+    }, 1000)
 }
-let time = 5;
+
+startTimer();
